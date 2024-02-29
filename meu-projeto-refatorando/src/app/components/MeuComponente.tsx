6f3a353ba4520662d8c57af2854cf7980894ffdf
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-02-29 13:40:06 -0300

    Aula criando meu primeiro componente

---
+++ meu-projeto-refatorando/src/app/components/MeuComponente.tsx
@@ -0,0 +1,10 @@
+function MeuComponente() {
+    return (
+        <div>
+            <h1>Meu Primeiro Componente</h1>
+            <p>Olá Mundo</p>
+        </div>
+    )
+}
+
+export default MeuComponente
